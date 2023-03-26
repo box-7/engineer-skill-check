6c0789c3f7dbf840371a445802ac2d2aeda8c31b
box-7 <hayainada1981@yahoo.co.jp>
2023-03-26 11:47:43 +0900

    Bootstrap導入

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,6 +1,6 @@
 module EmployeesHelper
   def sort_order(column, title)
     direction = params[:direction] == 'desc' ? 'asc' : 'desc'
-    link_to title, { sort: column, direction: direction }
+    link_to title, { sort: column, direction: direction }, class:"userList"
   end
 end
